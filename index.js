1963baab64360a7eeb57f173c88b64b8e9c57003
Victor Haynes <haynesvic@gmail.com>
2022-06-28 09:07:07 -0500

    commit tree 294e6aa7d77143fffabdd9550d00371a1de66969

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation){
+    return(Math.abs(42-pickupLocation))
+}
+
+function distanceFromHqInFeet(someValue) {
+    return(264*distanceFromHqInBlocks(someValue));
+    // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+    // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+  
+    // the return value of distanceFromHqInBlocks can then be used to calculate feet
+  }
+
+
+  function distanceTravelledInFeet(start, destination) {
+    //returns the number of feet traveled
+    return(Math.abs(destination-start)*264)
+  }
+
+  function calculatesFarePrice(start, destination){
+      let distanceInFeet = distanceTravelledInFeet(start, destination)
+      let fare;
+      if(distanceInFeet < 400){
+          fare = 0;
+          return (fare)
+      } else if (distanceInFeet >= 400 && distanceInFeet <= 2000){
+          fare = (distanceInFeet-400) * 0.02;
+          return(fare)
+      } else if (distanceInFeet > 2000 && distanceInFeet < 2500){
+          fare = 25
+          return(fare)
+      } else if (distanceInFeet > 2500){
+          fare = 'cannot travel that far'
+          return(fare)
+      } 
+  }
